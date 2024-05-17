88cdeb29657c5a72c3663f119486645f3c93c454
xsteadybcgo <xw4585@gmail.com>
2024-05-17 14:01:49 +0800

    fix: add log

--- projects/0vix/index.js
+++ projects/0vix/index.js
@@ -3,6 +3,8 @@ const {compoundExports2} = require('../helper/compound')
 const master0vix = "0x8849f1a0cB6b5D6076aB150546EddEe193754F1C";
 const oMATIC = "0xE554E874c9c60E45F1Debd479389C76230ae25A8";
 
+console.log('Test');
+
 module.exports = {
   hallmarks: [
     [Math.floor(new Date('2023-04-28')/1e3), 'Protocol was hacked!'],
